35696e999ededba12224c73abaa12471d685a333
Tim McIver <tim@timmciver.com>
2019-05-30 21:56:43 -0400

    Fix documentation for `Alternative` trait. (#7)

--- src/Typeclass/Alternative.php
+++ src/Typeclass/Alternative.php
@@ -4,16 +4,15 @@ namespace PhatCats\Typeclass;
 
 /**
  * A monoid on applicative functors.
- *
- * 
  */
 interface Alternative extends Applicative {
 
   /**
    * An associative binary operation 
    *
-   * @param $other :: m a : The other object being considered.
-   * @return :: m a : The result of "or"ing the two objects together.
+   * @param $left :: m a : The first action being considered.
+   * @param $right :: m a : The second action being considered.
+   * @return :: m a : The action resulting from "or"ing the two objects together.
    */
   function or($left, $right);
 
@@ -27,6 +26,7 @@ interface Alternative extends Applicative {
   /**
    * Repeatedly applies the action to return zero or more values.
    *
+   * @param $v :: m a : an action to apply.
    * @return :: m (LinkedList a)
    */
   function zeroOrMore($v);
@@ -34,6 +34,7 @@ interface Alternative extends Applicative {
   /**
    * Repeatedly applies the action to return one or more values.
    *
+   * @param $v :: m a : an action to apply.
    * @return :: m (LinkedList a)
    */
   function oneOrMore($v);
