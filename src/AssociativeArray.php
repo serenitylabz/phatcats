4da508fe78d48f1a6a4e76a4c52af2887a0d531e
Tim McIver <tim@timmciver.com>
2019-10-29 22:35:46 -0400

    AssociativeArray now implements ArrayAccess.
    * Reformat files.
    
    * Added test for accessing an element of `AssociativeArray` and implemented
    to make test pass.
    
    * Add tests for setting and unsetting an element in `AssociativeArray` which
    throw `UnsupportedOperationException` since we do not allow mutation.
    
    * Update comment to AssociativeArray.

--- src/AssociativeArray.php
+++ src/AssociativeArray.php
@@ -4,19 +4,26 @@ namespace PhatCats;
 
 use PhatCats\ObjectTypeclass\ObjectTraversable;
 
-// Just a wrapper around the native PHP array to allow for cool FP goodness.
-class AssociativeArray implements \IteratorAggregate {
-    use ObjectTraversable;
+class UnsupportedOperationException extends \Error {}
 
-    protected $array;
+/**
+ * AssociativeArray maps keys to values.  Keys and values can be any type of
+ * data. AssociativeArray's are immutable.  This means that some operations will
+ * throw an exception.  For example the mutating methods of the ArrayAccess
+ * interface throw UnsupportedOperationException.
+ */
+class AssociativeArray implements \ArrayAccess, \IteratorAggregate {
+  use ObjectTraversable;
 
-    public function __construct($array) {
-      $this->array = is_null($array) ? [] : $array;
-    }
+  protected $array;
 
-    // TODO: This is broken. It returns an instance of the given Monad wrapping
-    // a PHP _native_ array; not an AssociativeArray.
-    public function traverse(callable $f, $monad) {
+  public function __construct($array) {
+    $this->array = is_null($array) ? [] : $array;
+  }
+
+  // TODO: This is broken. It returns an instance of the given Monad wrapping
+  // a PHP _native_ array; not an AssociativeArray.
+  public function traverse(callable $f, $monad) {
 
 	// Initial value for the fold: an empty array wrapped in a default
 	// context.
@@ -25,38 +32,54 @@ class AssociativeArray implements \IteratorAggregate {
 	// Define the folding function.
 	$foldingFn = function ($acc, $curr) use ($f, $monad) {
 
-	    // Call $f on the current value of the array, $curr. The return
-	    // value should be a monadic value.
-            try {
-                $returnedMonad = $f($curr);
+      // Call $f on the current value of the array, $curr. The return
+      // value should be a monadic value.
+      try {
+        $returnedMonad = $f($curr);
 
-                // If the result is null, we fail.
-                if (is_null($returnedMonad)) {
-                    $returnedMonad = $monad->fail('The callable passed to `AssociativeArray::traverse` returned null.');
-                }
-            } catch (\Exception $e) {
-                $returnedMonad = $monad->fail('The callable passed to `AssociativeArray::traverse` threw an exception: ' . $e->getMessage());
-            }
+        // If the result is null, we fail.
+        if (is_null($returnedMonad)) {
+          $returnedMonad = $monad->fail('The callable passed to `AssociativeArray::traverse` returned null.');
+        }
+      } catch (\Exception $e) {
+        $returnedMonad = $monad->fail('The callable passed to `AssociativeArray::traverse` threw an exception: ' . $e->getMessage());
+      }
 
-	    // Put the value wrapped by the above monadic value in the array
-	    // held by the accumulator, $acc, to get the new accumulator.
-	    $newAcc = $returnedMonad->flatMap(function ($newVal) use ($acc) {
+      // Put the value wrapped by the above monadic value in the array
+      // held by the accumulator, $acc, to get the new accumulator.
+      $newAcc = $returnedMonad->flatMap(function ($newVal) use ($acc) {
 		return $acc->map(function ($arr) use ($newVal) {
-		    $arr[] = $newVal;
-		    return $arr;
+          $arr[] = $newVal;
+          return $arr;
 		});
-	    });
+      });
 
-	    return $newAcc;
+      return $newAcc;
 	};
 
 	// Do the fold.
 	$result = array_reduce($this->array, $foldingFn, $init);
 
 	return $result;
-    }
+  }
+
+  public function offsetExists($offset): bool {
+    return array_key_exists($offset, $this->array);
+  }
+
+  public function offsetGet($offset) {
+    return $this->array[$offset];
+  }
+
+  public function offsetSet($offset, $value) {
+    throw new UnsupportedOperationException();
+  }
+
+  public function offsetUnset($offset) {
+    throw new UnsupportedOperationException();
+  }
 
   public function getIterator() : \Traversable {
-      return new \ArrayIterator($this->array);
-    }
+    return new \ArrayIterator($this->array);
+  }
 }
--- test/Array/ArrayTest.php
+++ test/Array/ArrayTest.php
@@ -4,84 +4,106 @@ use PHPUnit\Framework\TestCase;
 use PhatCats\AssociativeArray;
 use PhatCats\Either\Either;
 use PhatCats\Either\Monad\RightFavoringEitherMonad;
+use PhatCats\UnsupportedOperationException;
 
 class ArrayTest extends TestCase {
 
-    private $monad;
+  private $monad;
 
-    protected function setUp() {
-        $this->monad = new RightFavoringEitherMonad();
-    }
+  protected function setUp() {
+    $this->monad = new RightFavoringEitherMonad();
+  }
 
-    public function testTraverseSuccessForArrayOfInt() {
+  public function testTraverseSuccessForArrayOfInt() {
 
 	$dividend = 12;
 	$divisors = [2, 4, 6];
 	$intsArray = new AssociativeArray($divisors);
 	$eitherResults = $intsArray->traverse(function ($i) use ($dividend) {
-	    return divide($dividend, $i);
+      return divide($dividend, $i);
 	}, $this->monad);
 	$expected = Either::fromValue([6, 3, 2]);
 
 	$this->assertEquals($expected, $eitherResults);
-    }
+  }
 
-    public function testTraverseFailureForArrayOfInt() {
+  public function testTraverseFailureForArrayOfInt() {
 
 	$dividend = 12;
 	$divisors = [2, 0, 6];
 	$intsArray = new AssociativeArray($divisors);
 	$eitherResults = $intsArray->traverse(function ($i) use ($dividend) {
-	    return divide($dividend, $i);
+      return divide($dividend, $i);
 	}, $this->monad);
 	$expected = Either::left('Division by zero!');
 
 	$this->assertEquals($expected, $eitherResults);
-    }
+  }
 
-    public function testTraverseForEmptyArray() {
+  public function testTraverseForEmptyArray() {
 
 	$arr = new AssociativeArray([]);
 	$eitherResult = $arr->traverse(function ($ignore) {
-	    throw new \Exception('This should not affect the traversal as it should not be called!');
+      throw new \Exception('This should not affect the traversal as it should not be called!');
 	}, $this->monad);
 	$expected = Either::fromValue([]);
 
 	$this->assertEquals($expected, $eitherResult);
-    }
+  }
 
-    public function testTraverseForThrownException() {
+  public function testTraverseForThrownException() {
 
-        $intsArray = new AssociativeArray([2, 0, 6]);
-        $eitherResults = $intsArray->traverse(function ($i) {
-            if ($i == 0) {
-                throw new \Exception('Found zero!');
-            } else {
-                return Either::fromValue($i);
-            }
-        }, $this->monad);
-        $expected = $this->monad->fail();
+    $intsArray = new AssociativeArray([2, 0, 6]);
+    $eitherResults = $intsArray->traverse(function ($i) {
+      if ($i == 0) {
+        throw new \Exception('Found zero!');
+      } else {
+        return Either::fromValue($i);
+      }
+    }, $this->monad);
+    $expected = $this->monad->fail();
 
-        $this->assertInstanceOf(PhatCats\Either\Left::class, $expected);
-    }
+    $this->assertInstanceOf(PhatCats\Either\Left::class, $expected);
+  }
 
-    public function testTraverseForReturningNull() {
+  public function testTraverseForReturningNull() {
 
-        $intsArray = new AssociativeArray([2, 0, 6]);
-        $eitherResults = $intsArray->traverse(function ($ignore) {
-            return null;
-        }, $this->monad);
-        $expected = $this->monad->fail();
+    $intsArray = new AssociativeArray([2, 0, 6]);
+    $eitherResults = $intsArray->traverse(function ($ignore) {
+      return null;
+    }, $this->monad);
+    $expected = $this->monad->fail();
 
-        $this->assertInstanceOf(PhatCats\Either\Left::class, $expected);
-    }
+    $this->assertInstanceOf(PhatCats\Either\Left::class, $expected);
+  }
 
-    public function testHandlingOfNullArgument() {
-      $arr = new AssociativeArray(null);
-      $expected = Either::fromValue([]);
+  public function testHandlingOfNullArgument() {
+    $arr = new AssociativeArray(null);
+    $expected = Either::fromValue([]);
 
-      $this->assertEquals($arr->sequence($this->monad), $expected);
-    }
+    $this->assertEquals($arr->sequence($this->monad), $expected);
+  }
+
+  public function testArrayAccessGet() {
+    $arr = new AssociativeArray(["foo" => 1, "bar" => 2]);
+    $actual = $arr["foo"];
+    $expected = 1;
+    $this->assertEquals($actual, $expected);
+  }
+
+  public function testArrayAccessSet() {
+    $this->expectException(UnsupportedOperationException::class);
+
+    $arr = new AssociativeArray(["foo" => 1]);
+    $arr["bar"] = 2;
+  }
+
+  public function testArrayAccessUnset() {
+    $this->expectException(UnsupportedOperationException::class);
+
+    $arr = new AssociativeArray(["foo" => 1, "bar" => 2]);
+    unset($arr["bar"]);
+  }
 
   public function testForEach() {
     $arr = ['1' => 1,
@@ -103,11 +125,11 @@ class ArrayTest extends TestCase {
  * @return Either number; Left if the divisor is zero, Right otherwise.
  */
 function divide($x, $y) {
-    if ($y == 0) {
+  if ($y == 0) {
 	$eitherResult = Either::left('Division by zero!');
-    } else {
+  } else {
 	$eitherResult = Either::fromValue($x/$y);
-    }
+  }
 
-    return $eitherResult;
+  return $eitherResult;
 }
